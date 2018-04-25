2c97a370d1d88839792a286827962cd8d1913d4f
avichakladar <axc161930@utdallas.edu>
2018-04-24 21:42:58 -0500

    updated cc file

--- program6.cc
+++ program6.cc
@@ -7,7 +7,7 @@
 #include <iostream>
 #include "cdk.h"
 #include <fstream>
-
+#include "program6.h"
 
 #define MATRIX_WIDTH 3
 #define MATRIX_HEIGHT 5
@@ -24,10 +24,14 @@ int main()
   CDKSCREEN	*cdkscreen;
   CDKMATRIX     *myMatrix;           // CDK Screen Matrix
 
-  ifstream binInfile ("cs3377.bin", ios::in | ios::binary);
-
+  BinaryFileRecord *record = new BinaryFileRecord();
+  BinaryFileHeader *header = new BinaryFileHeader();
   
-
+  ifstream binInfile ("cs3377.bin", ios::in | ios::binary);
+  binInfile.read((char *) header, sizeof(BinaryFileHeader));
+  
+  char buff[1024];
+  
   // Remember that matrix starts out at 1,1.
   // Since arrays start out at 0, the first entries
   // below ("R0", and "C0") are just placeholders
@@ -35,7 +39,6 @@ int main()
   // Finally... make sure your arrays have enough entries given the
   // values you choose to set for MATRIX_WIDTH and MATRIX_HEIGHT
   // above.
-
   const char 		*rowTitles[] = {"R0", "a", "b", "c", "d", "e"};
   const char 		*columnTitles[] = {"C0", "a", "b", "c", "C4", "C5"};
   int		boxWidths[] = {BOX_WIDTH, BOX_WIDTH, BOX_WIDTH, BOX_WIDTH, BOX_WIDTH, BOX_WIDTH};
@@ -71,7 +74,16 @@ int main()
   /*
    * Dipslay a message
    */
-  setCDKMatrixCell(myMatrix, 2, 2, "Test Message");
+  sprintf(buff, "Magic: 0x%X", header -> magicNumber);
+  setCDKMatrixCell(myMatrix, 1, 1, buff);
+  drawCDKMatrix(myMatrix, true);    /* required  */
+  
+  sprintf(buff, "Version: %u", header -> versionNumber);
+  setCDKMatrixCell(myMatrix, 1, 2, buff);
+  drawCDKMatrix(myMatrix, true);    /* required  */
+  
+  sprintf(buff, "NumRecords: %lu", header -> numRecords);
+  setCDKMatrixCell(myMatrix, 1, 3, buff);
   drawCDKMatrix(myMatrix, true);    /* required  */
 
   /* So we can see results, pause until a key is pressed. */
